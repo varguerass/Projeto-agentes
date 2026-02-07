46125f6e96a53c72c819887135c2438514dbb004
Vargas <vargasr.mariac@gmail.com>
2026-02-07 11:19:45 -0300

    projeto finalizado, console funcionando

--- Main.js
+++ Main.js
@@ -1,3 +1,6 @@
+// Importa a biblioteca para ler o que você digita no terminal
+const prompt = require('readline-sync').question;
+
 const Agentes = [
   { nome: "Sage", xp: 850 },
   { nome: "Reyna", xp: 2500 },
@@ -8,18 +11,34 @@ const Agentes = [
   { nome: "Chamber", xp: 8000 },
   { nome: "Cypher", xp: 12000 }
 ];
-console.log("Escolha seu agente: ");
+
+console.log("=== Escolha seu agente ===");
 for (let i = 0; i < Agentes.length; i++) {
   console.log(`${i + 1} - ${Agentes[i].nome}`);
 }
 
+const entrada = prompt("Digite o numero do agente: ");
+console.log("Você digitou:", entrada);
+const escolha = parseInt(entrada);
+
+const agenteEscolhido = Agentes[escolha - 1];
+
 function definirRank(xp) {
-  if (xp < 1000) return "Ferro";
-  if (1001< xp < 2000) return "Bronze";
-  if (2001< xp < 5000) return "Prata";
-  if (5001< xp < 7000 ) return "Ouro";
-  if (7001 < xp < 8000) return "Platina"
-  if (8001< xp < 9001) return "Ascendente"
-  if (9001 < xp < 10001) return "Imortal"
+  if (xp <= 1000) return "Ferro";
+  if (xp <= 2000) return "Bronze";
+  if (xp <= 5000) return "Prata";
+  if (xp <= 7000) return "Ouro";
+  if (xp <= 8000) return "Platina";
+  if (xp <= 9000) return "Ascendente";
+  if (xp <= 10000) return "Imortal";
   return "Radiante";
+}
+
+if (agenteEscolhido) {
+  const nivel = definirRank(agenteEscolhido.xp);
+  console.log("--------------------------------------------------");
+  console.log(`Você escolheu o agente ${agenteEscolhido.nome}, ele está no nível ${nivel}`);
+  console.log("--------------------------------------------------");
+} else {
+  console.log("Opção inválida! Escolha um número de 1 a 8.");
 }
